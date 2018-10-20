0c11ad92fc4395dd561697c9bffb8c7ab7e4af82
gdpeters <43419592+gdpeters@users.noreply.github.com>
2018-10-20 10:17:02 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,31 @@
+
+//add #include headers as needed
+
+//add unions, structs, and global variables
+
+
+int main() {
+
+/** Read file and load memory array */
+
+/** Initiate registers, instruction specifier, operand specifier, and operand */
+
+/** Initiate an output file to write to if we choose this alternative route as opposed to screen display */
+
+/** Iterate through memory */
+
+    /** Load and determine instruction specifier and increment PC */
+    
+    /** Load operand specifier, extract register and addressing mode, update operand, and increment PC */
+    
+    /** Execute instruction by calling function (from other .cpp file or explicitly written) */
+    
+    /** Display register values to the screen or output file. Call function to do this. */
+    
+    
+ /** What else are we missing here? */   
+ 
+ 
+    
+  return 0;
+}
