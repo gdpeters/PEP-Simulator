16c22703916c01194c6ba55934a8b4052b951196
gdpeters <43419592+gdpeters@users.noreply.github.com>
2018-11-06 14:36:18 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -63,11 +63,22 @@ union tempUnit {
 	unsigned int full : 16;
 } tempValue, tempAddr;
 
+void printRegisters(registers rgstr)
+{
+	cout<<"Current Registers [ \n";
+	cout<<"Program Counter: "<< rgstr.pC;
+	cout<<"\nAccumulator: "<<rgstr.rA.full;
+	cout<<"\nIndex: "<< rgstr.rX.full;
+	cout<<"\nStack Pointer: " << rgstr.sP;		//How are we updating SP. I don't see a change in PEP/8.
+	cout<<"\nInstruction Specifier: "<<rgstr.iSPec.full;
+	cout<<"\nOperand Specifier: "<<rgstr.oSpec.full;
+	cout<<"\nOperand: "<< rgstr.operand.full<<" ]\n\n"; 
+}
 int main() {
 	ifstream readFile;               
 	readFile.open("text.txt", ios::in);       
 	unsigned int mainMem[50];            
-	unsigned int tempHex;                //For retrieving each hex value from file
+	unsigned int tempHex;                
 	int count = 0;                    
 
 	while (readFile >> hex >> tempHex)         //Load 1-byte from input file to variable
@@ -79,7 +90,7 @@ int main() {
 	reg.pC = 0;            //Initiate program counter to 0
 	reg.iSpec.full = mainMem[reg.pC++];    //Load first memory array cell into the instruction specifier
 													
-	while (reg.iSpec.full != 0)        //While instruction specifier is not STOP
+	while (reg.iSpec.full != 0)        
 	{
 		if (reg.iSpec.arith.instr4 > 3) //Update operand specifier for I/O and Arithmetic instructions
 		{
@@ -96,175 +107,165 @@ int main() {
 			
 		switch (reg.iSpec.logArith.instr4)    //Check first nibble to extract instruction code
 		{
-		case 1:
-			switch (reg.instrSpecifier.unary.instr7)    //Unary no operand
-			{
-			case 12: //Bitwise invert
-				if (reg.instrSpecifier.unary.r == 0)
-					reg.rA16 = reg.rA16 ^ ((1 << 16) - 1); //is there a shorter way to write this?
-				else
-					reg.rX16 = reg.rX16 ^ ((1 << 16) - 1); //is there a shorter way to write this?
-				break;
-			case 14:
-					
-				if (reg.instrSpecifier.unary.r == 0)
-					reg.rA16 <<= 1;    //Arithmetic shift left Accumulator
-				else
-					reg.rX16 <<= 1;    //Arithmetic shift left Index
-				break;
-			case 15:
-				if (reg.instrSpecifier.unary.r == 0)
-					reg.rA16 >>= 1;    //Arithmetic shift right Accumulator
-				else
-					reg.rX16 >>= 1;     //Arithmetic shift right Index
-				break;
-			default: cout << "Error: Case 1";
-			}
-			break;
-
-		case 2: //unary no operand
-			switch (reg.instrSpecifier.unary.instr7)
-			{
-			case 16: //Rotate left
-				if (reg.instrSpecifier.unary.r == 0)
-					reg.rA16 = (reg.rA16 << 1) + (reg.rA16 >> 15);
-				else
-					reg.rX16 = (reg.rX16 << 1) + (reg.rX16 >> 15);
-				break;
-			case 17: //Rotate right
-				if (reg.instrSpecifier.unary.r == 0)
-					reg.rA16 = (reg.rA16 >> 1) + (reg.rA16 << 15);
-				else
-					reg.rX16 = (reg.rX16 >> 1) + (reg.rX16 << 15);
-				break;
-			default: cout << "Error: Case 2";
-
-			}
-			break;
-
-		case 3:
-			switch (reg.instrSpecifier.ioTraps.instr5)
-			{
-			case 6:    	//Decimal input
-				//int t;	//why can't 'cin' take in unsigned 16-bit int?
-				//cin >> t; //NEED TO ADJUST FOR ASCII CHARACTER
-				tempValue.full = 124;						//assign user input to temp value
-				tempAddr.byteL = mainMem[reg.pC++];		//assign left byte of address to temp address
-				tempAddr.byteR = mainMem[reg.pC++];		//assign right byte of address to temp address
-				mainMem[tempAddr.full] = tempValue.byteL;
-				mainMem[tempAddr.full + 1] = tempValue.byteR;
-				tempValue.full = 0;		//reset temporary value
-				tempAddr.full = 0;		//reset temporary address
-				break;
-			case 7:		//Decimal output
-				if (reg.instrSpecifier.ioTraps.aaa == 0)
+			case 1:
+				switch (reg.iSpec.unary.instr7)    //Unary no operand
 				{
-					tempValue.byteL = mainMem[reg.pC++];
-					tempValue.byteR = mainMem[reg.pC++];
-					cout << tempValue.full;
-					tempValue.full = 0;
-				}
-				else
-				{
-					tempAddr.byteL = mainMem[reg.pC++];
-					tempAddr.byteR = mainMem[reg.pC++];
-
-					tempValue.byteL = mainMem[tempAddr.full];
-					tempValue.byteR = mainMem[tempAddr.full + 1];
-
-					cout << tempValue.full;
-					tempValue.full = 0;
-					tempAddr.full = 0;
+					case 12: //Bitwise invert
+						if (reg.iSpec.unary.r1 == 0)
+							reg.rA.full = reg.rA.full ^ ((1 << 16) - 1); //is there a shorter way to write this?
+						else
+							reg.rX.full = reg.rX.full ^ ((1 << 16) - 1); //is there a shorter way to write this?
+						break;
+					case 14: //Arithmetic shift left
+						if (reg.iSpec.unary.r1 == 0)
+							reg.rA.full <<= 1;    
+						else
+							reg.rX.full <<= 1;   
+						break;
+					case 15: //Arithmetic shift right
+						if (reg.iSpec.unary.r1 == 0)
+							reg.rA.full >>= 1;    
+						else
+							reg.rX.full >>= 1;    
+						break;
+					default: cout << "Error: Case 1";
 				}
 				break;
-			default: cout << "Error: Case 3";
-			}
-			break;
 
-		case 4: 		//Character input
-			reg.operSpecifier.byteL = mainMem[reg.pC++];
-			reg.operSpecifier.byteR = mainMem[reg.pC++];
-			//int t;	//why can't 'cin' take in unsigned 16-bit int?
-			//cin >> t; //NEED TO ADJUST FOR ASCII CHARACTER
-			reg.operand.full = 56;
-			mainMem[reg.operSpecifier.full] = reg.operand.byteL;
-			mainMem[reg.operSpecifier.full + 1] = reg.operand.byteR;
-			break;
+			case 2: 
+				switch (reg.iSpec.unary.instr7) //Unary no operand
+				{
+					case 16: //Rotate left
+						if (reg.iSpec.unary.r1 == 0)
+							reg.rA.full = (reg.rA.full << 1) + (reg.rA.full >> 15);
+						else
+							reg.rX.full = (reg.rX.full << 1) + (reg.rX.full >> 15);
+						break;
+					case 17: //Rotate right
+						if (reg.iSpec.unary.r1 == 0)
+							reg.rA.full = (reg.rA.full >> 1) + (reg.rA.full << 15);
+						else
+							reg.rX.full = (reg.rX.full >> 1) + (reg.rX.full << 15);
+						break;
+					default: cout << "Error: Case 2";
 
+				}
+				break;
 
+			case 3:
+				switch (reg.iSpec.ioTrap.instr5)
+				{
+					case 6:	//Decimal input
+				//why can't 'cin' take in unsigned 16-bit int?
+				//NEED TO ADJUST FOR ASCII CHARACTER
+						int decIn;
+						cin>>decIn;
+						tempValue.full = decIn;						
+						tempAddr.msd8 = mainMem[reg.pC++];		
+						tempAddr.lsd8 = mainMem[reg.pC++];		
+						mainMem[tempAddr.full] = tempValue.msd8;
+						mainMem[tempAddr.full + 1] = tempValue.lsd8;
+						tempValue.full = 0;		//reset temporary value
+						tempAddr.full = 0;		//reset temporary address
+						break;
+					case 7:		//Decimal output
+						if (reg.iSpec.ioTrap.aaa3 == 0)
+						{
+							tempValue.msd8 = mainMem[reg.pC++];
+							tempValue.lsd8 = mainMem[reg.pC++];
+							cout << tempValue.full;
+							tempValue.full = 0;
+						}
+						else
+						{
+							tempAddr.msd8 = mainMem[reg.pC++];
+							tempAddr.lsd8 = mainMem[reg.pC++];
+							tempValue.msd8 = mainMem[tempAddr.full];
+							tempValue.lsd8 = mainMem[tempAddr.full + 1];
+							cout << tempValue.full;
+							tempValue.full = 0;
+							tempAddr.full = 0;
+						}
+						break;
+					default: cout << "Error: Case 3";
+				}
+				break;
 
-		case 5:		//Character output
-			cout << reg.operand.full; //NEED TO ADJUST FOR CHARACTER OUTPUT
-			break;
+			case 4:		//Character input
+				reg.operand.full = mainMem[reg.oSpec.full]; //only 1-byte?
+				break;
+				
+			case 5:		//Character output
+				cout << reg.operand.full; //NEED TO ADJUST FOR CHARACTER OUTPUT
+				break;
 			//no case 6
-		case 7:
-			if (reg.instrSpecifier.logArith.r == 0)    //Add to r
-				reg.rA16 += reg.operand.full;
-			else
-				reg.rX16 += reg.operand.full;
-			break;
-		case 8:
-			if (reg.instrSpecifier.logArith.r == 0)    //Subtract from r
-				reg.rA16 -= reg.operand.full;
-			else
-				reg.rX16 -= reg.operand.full;
-			break;
-		case 9: 
-			if (reg.instrSpecifier.logArith.r == 0)
-				reg.rA16 = reg.rA16 & reg.operand.full;    //Bitwise AND
-			else
-				reg.rX16 = reg.rX16 & reg.operand.full;
-			break;
-		case 10:
-			if (reg.instrSpecifier.logArith.r == 0)
-				reg.rA16 = reg.rA16 | reg.operand.full;    //Bitwise OR
-			else
-				reg.rX16 = reg.rX16 | reg.operand.full;
-			break;
+			case 7:		//Add to register
+				if (reg.iSpec.arith.r1 == 0)    
+					reg.rA.full += reg.operand.full;
+				else
+					reg.rX.full += reg.operand.full;
+				break;
+			case 8:		//Subtract from register
+				if (reg.iSpec.arith.r1 == 0)    
+					reg.rA.full -= reg.operand.full;
+				else
+					reg.rX.full -= reg.operand.full;
+				break;
+			case 9: 	//Bitwise AND
+				if (reg.iSpec.arith.r1 == 0)
+					reg.rA.full &= reg.operand.full;    
+				else
+					reg.rX.full &= reg.operand.full;
+				break;
+			case 10:	//Bitwise OR
+				if (reg.iSpec.arith.r1 == 0)
+					reg.rA.full |= reg.operand.full;    
+				else
+					reg.rX.full |= reg.operand.full;
+				break;
 			//no case 11
-		case 12:
-			if (reg.instrSpecifier.logArith.r == 0)    //Load r from memory
-				reg.rA16 = reg.operand.full;
-			else
-				reg.rX16 = reg.operand.full;
-			break;
-		case 13: 
-			if (reg.instrSpecifier.logArith.r == 0)    //Load byte from memory
-				reg.rA8R = reg.operand.byteR;
-			else
-				reg.rX8R = reg.operand.byteR;
-			break;
-		case 14: 
-			if (reg.instrSpecifier.logArith.r == 0)    //Store r to memory
-			{
-				reg.operand.full = reg.rA16;
-				mainMem[reg.operSpecifier.full] = reg.rA8L;
-				mainMem[reg.operSpecifier.full + 1] = reg.rA8R;
-			}	
-			else
-			{
-				reg.operand.full = reg.rX16;
-				mainMem[reg.operSpecifier.full] = reg.rX8L;
-				mainMem[reg.operSpecifier.full + 1] = reg.rX8R;
-			}
-			break;
-		case 15:
-			if (reg.instrSpecifier.logArith.r == 0)    //Store byte to memory
-			{
-				reg.operand.full = reg.rA8R;
-				mainMem[reg.operSpecifier.full] = reg.rA8R;
-			}
-			else
-			{
-				reg.operand.full = reg.rX8R;
-				mainMem[reg.operSpecifier.full] = reg.rX8R;
-			}
-			break;
+			case 12:	//Load register from memory
+				if (reg.iSpec.arith.r1 == 0)    
+					reg.rA.full = reg.operand.full;
+				else
+					reg.rX.full = reg.operand.full;
+				break;
+			case 13: 	//Load byte from memory
+				if (reg.iSpec.arith.r1 == 0)   
+					reg.rA.lsd8 = reg.operand.lsd8;
+				else
+					reg.rX.lsd8 = reg.operand.lsd8;
+				break;
+			case 14: 	//Store register to memory
+				if (reg.iSpec.arith.r1 == 0)   
+				{
+					reg.operand.full = reg.rA.full;
+					mainMem[reg.oSpec.full] = reg.operand.msd8;
+					mainMem[reg.oSpec.full + 1] = reg.operand.lsd8;
+				}	
+				else
+				{
+					reg.operand.full = reg.rX.full;
+					mainMem[reg.oSpec.full] = reg.operand.msd8;
+					mainMem[reg.oSpec.full + 1] = reg.operand.lsd8;
+				}
+				break;
+			case 15:	//Store byte register to memory
+				if (reg.iSpec.arith.r1 == 0)    
+				{
+					reg.operand.full = reg.rA.lsd8;
+					mainMem[reg.oSpec.full] = reg.operand.lsd8;
+				}
+				else
+				{
+					reg.operand.full = reg.rX.lsd8;
+					mainMem[reg.oSpec.full] = reg.rX.lsd8;
+				}
+				break;
 		}
-				/** Display register values to the screen or output file (call function to do this) */
-			cout <<"IS: "<< reg.instrSpecifier.full << " OS: " << reg.operSpecifier.full << " OP:  " << reg.operand.full << " PC: " << reg.pC << " A: " << reg.rA16 << " X: " << reg.rX16 << " SP: " << reg.sP;
-			reg.instrSpecifier.full = mainMem[reg.pC++];
-		}
-		//clear operand & operand specifier display
-		return 0;
+		
+		printRegisters(reg);
+		reg.iSpec.full = mainMem[reg.pC++];
 	}
+		return 0;
+}
